ac188ae74376f11df37980af4319593bcfcd3024
itzownway <itzzsanjaykumardas@gmail.com>
2026-06-23 12:06:08 +0530

    practice

--- 1.Basic/5.Function/even_odd.py
+++ 1.Basic/5.Function/even_odd.py
@@ -1,9 +1,13 @@
 def OddEven():
-    num = int(input("Enter a number: "))
-    if num % 2 == 0:
-        print(f"{num} is an even number.")
-    else:    
-        print(f"{num} is an odd number.")
-
+    
+    try:
+        num = int(input("Enter a number: "))
+        if num % 2 == 0:
+            print(f"{num} is an even number.")
+        else:    
+            print(f"{num} is an odd number.")
+        
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")    
 
 OddEven()
